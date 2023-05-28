22fff3de79a3386d5474b460c02f580db51a2bb5
Mirzakhan Aliyev <jurimuller.cloud@gmail.com>
2023-05-28 19:17:31 +0400

    Add Json headers to Blog model

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,8 +1,20 @@
+using Newtonsoft.Json;
+
 namespace Md2Medium.Models;
 
+[JsonObject]
 public class Blog {
+    [JsonProperty("repoAddress")]
     public string RepoAddress {get; set;}
+
+    [JsonProperty("branch")]
     public string Branch {get; set;}
+
+
+    [JsonProperty("filePath")]
     public string FilePath {get; set;}
+    
+    
+    [JsonProperty("title")]
     public string Title {get; set;}
 }
